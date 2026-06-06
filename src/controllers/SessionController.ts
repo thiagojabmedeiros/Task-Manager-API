bf57fbc876a6975503de4463d956a3d9aca0b497
Thiago Medeiros <thiagojabm@gmail.com>
2026-06-06 15:44:18 -0300

    creating session functionality

--- src/controllers/SessionController.ts
+++ src/controllers/SessionController.ts
@@ -1,12 +1,34 @@
 import { Request, Response } from "express"
+import { compare } from "bcrypt"
 import z from "zod"
+
 
 import AppError from "../utils/AppError"
 import User from "../models/User"
 
 class SessionController {
     async create(request: Request, response: Response) {
-        return response.status(201).json({ message: "ok!"})
+        const bodySchema = z.object({
+            email: z.email(),
+            password: z.string()
+        })
+        const { email, password } = bodySchema.parse(request.body)
+
+        const user = await User.findOne({
+            where: {
+                email: email
+            }
+        })
+        if (!user) {
+            throw new AppError("invalid email or password", 401)
+        }
+
+        const pwd = await compare(password, user.toJSON().password)
+        if (!pwd) {
+            throw new AppError("invalid email or password", 401)
+        }
+
+        return response.status(201).json(user)
     }
 }
 
