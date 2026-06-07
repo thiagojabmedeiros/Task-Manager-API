f3528edea7f89489d85703a62a28c34d1f59d131
Thiago Medeiros <thiagojabm@gmail.com>
2026-06-07 15:09:13 -0300

    creating team method

--- src/controllers/TeamController.ts
+++ src/controllers/TeamController.ts
@@ -2,9 +2,7 @@ import { Request, Response } from "express";
 import z from "zod"
 import AppError from "../utils/AppError";
 
-import User from "../models/User";
 import Team from "../models/Team";
-import { th } from "zod/locales";
 
 class TeamController {
     async create(request: Request, response: Response) {
@@ -13,6 +11,7 @@ class TeamController {
             description: z.string().nullish()
         })
         const { name, description } = bodySchema.parse(request.body)
+
         const exists = await Team.findOne({
             where: {
                 name: name
@@ -21,6 +20,9 @@ class TeamController {
         if (exists) {
             throw new AppError("there is another team with the same name")
         }
+
+        const team = await Team.create({ name, description })
+
         return response.status(201).json({ message: "ok!" })
     }
 }
