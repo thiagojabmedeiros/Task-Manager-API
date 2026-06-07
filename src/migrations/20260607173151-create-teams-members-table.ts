418604d2b35cd2c6e9714d157b133d27b546cec2
Thiago Medeiros <thiagojabm@gmail.com>
2026-06-07 14:41:16 -0300

    creating teams members migration

---
+++ src/migrations/20260607173151-create-teams-members-table.ts
@@ -0,0 +1,39 @@
+'use strict';
+
+import { DataTypes, QueryInterface } from "sequelize";
+
+/** @type {import('sequelize-cli').Migration} */
+export default {
+  async up (queryInterface: QueryInterface) {
+    await queryInterface.createTable("teams", {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        allowNull: false,
+      },
+      team_id: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        references: { key: "id", model: "teams" },
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE"
+      },
+      user_id: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        references: { key: "id", model: "users"},
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE"
+      },
+      created_at: {
+        type: DataTypes.DATE,
+        allowNull: false
+      },
+    })
+  },
+
+  async down (queryInterface: QueryInterface) {
+    await queryInterface.dropTable("teams")
+  }
+};
